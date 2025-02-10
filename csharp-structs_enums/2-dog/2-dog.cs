a25c5020c7d0b16b18e3908df52dabb9f3134b9d
Akunyiba Chimdalu Divinefavour <c.akunyiba@alustudent.com>
2025-02-10 19:47:43 +0200

    done

---
+++ csharp-structs_enums/2-dog/2-dog.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+enum Rating
+{
+    Good,      // 0
+    Great,     // 1
+    Excellent   // 2
+}
+
+struct Dog
+{
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+
+    // Constructor that takes parameters
+    public Dog(string name, float age, string owner, Rating rating)
+    {
+        this.name = name;
+        this.age = age;
+        this.owner = owner;
+        this.rating = rating;
+    }
+}
